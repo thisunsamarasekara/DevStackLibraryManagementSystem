2faafe9919ef979375ea3c845b29e0de7b57ca4e
thameeramalinga <thameera1002@gmail.com>
2024-12-31 19:53:37 +0530

    main class updated

--- src/main/java/com/librarymanagementsystem/Main.java
+++ src/main/java/com/librarymanagementsystem/Main.java
@@ -1,20 +1,99 @@
 package com.librarymanagementsystem;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        Library library = new Library();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<LibraryItem> libraryItems = LibraryIO.loadItemsFromFile("itemlist.lms");
+        for(LibraryItem item:libraryItems){
+            library.addItem(item);
         }
+
+        List<User> users = LibraryIO.loadUserListFromFile("userlist.lms");
+        for(User user:users){
+            library.addUser(user);
+        }
+
+        Map<String,String> borrowedItems = LibraryIO.loadBorrowedItemsFromFile("borroweditems.lms");
+        for(Map.Entry<String,String> borrowedItem:borrowedItems.entrySet()){
+            library.getBorrowedItems().put(borrowedItem.getKey(),borrowedItem.getValue());
+        }
+
+        System.out.println("Please find the list of all library items");
+        library.getLibraryItems().forEach(item-> System.out.println(item.getTitle()+"\t"+item.getAuthor()+"\t"+item.getSerialNumber()));
+        System.out.println("-----------------------------------------");
+
+
+        System.out.println("Please find the list of all users");
+        library.getUserList().forEach(user -> System.out.println(user.getName()));
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Please find the list of all the borrowed items from the library");
+        library.getBorrowedItems().forEach((item,user)-> System.out.println(item+" : "+user));
+        System.out.println("-----------------------------------------");
+
+        boolean exit = false;
+        L1:while (!exit){
+            System.out.println("Enter the main option");
+            System.out.println("1. Need to create a new item");
+            System.out.println("2. Need to create a new User");
+            System.out.println("3. User need to borrow an item");
+            System.out.println("4. User need to return an item");
+            System.out.println("5. Exit the application");
+
+            BufferedReader mainOption = new BufferedReader(new InputStreamReader(System.in));
+            int mainOptionStr;
+            try {
+                mainOptionStr = Integer.parseInt(mainOption.readLine());
+            } catch (NumberFormatException | IOException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+            if(mainOptionStr == 1){
+                System.out.println("Which item do you need to create ?");
+                System.out.println("1. Book");
+                System.out.println("2. Magazine");
+
+                BufferedReader createItemType = new BufferedReader(new InputStreamReader(System.in));
+                int createItemTypeStr;
+                try {
+                   createItemTypeStr = Integer.parseInt(createItemType.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                if(createItemTypeStr == 1){
+                    System.out.println("Please enter the book name you want to create");
+                    String bookNameStr = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                    System.out.println("Please enter the book author you want to create");
+                    String bookAuthorStr = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                    System.out.println("Please enter the book serial number you want to create");
+                    String bookSerialNumberStr = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                    for(LibraryItem li:library.getLibraryItems()){
+                        if(Objects.equals(li.getSerialNumber(),bookSerialNumberStr)){
+                            System.out.println("This serial number is already entered !");
+                            continue L1;
+                        }
+                    }
+                    LibraryItem createBook = new Book(bookNameStr,bookAuthorStr,bookSerialNumberStr);
+                    library.addItem(createBook);
+                }else if(createItemTypeStr == 2){
+                    //please implement adding magazine section
+                }
+
+            } else if (mainOptionStr == 5) {
+                exit = true;
+            }
+        }
+        LibraryIO.saveItemToFile(library.getLibraryItems(),"itemlist.lms");
     }
 }
